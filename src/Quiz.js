7e64289410972015874049db8b5c2c193195b2d9
Scott Mahoney <scott.mahoney@moxieusa.com>
2019-07-07 13:38:44 -0400

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,6 +14,10 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    hanldeResetClick(){
+        this.setState({quiz_position : 1})
+    }
+
     showNextQuestion(){
         this.setState((state)=>{
             return {quiz_position: state.quiz_position + 1}
