0a352ef9f82104cde23e0e31546d2dd1ef1e6965
Scott Mahoney <scott.mahoney@moxieusa.com>
2019-07-14 13:43:08 -0400

    Updated to include Correct/InCorrect display

--- src/QuizHeader.js
+++ src/QuizHeader.js
@@ -3,6 +3,16 @@ import React, {Component} from 'react'
 class QuizHeader extends Component{
 
 
+    getResult(){
+        return (
+            this.props.quiz_position <= this.props.quiz.NumberOfQuestions-1 ? 
+            this.props.quiz.Questions[this.props.quiz_position-1].IsCorrect ?
+            <span style={{color:'green'}}>Correct</span>
+            :<span style={{color:'red'}}>Incorrect</span>
+            : null
+        )
+    }
+
     render(){
         return(
             <div>
@@ -10,6 +20,13 @@ class QuizHeader extends Component{
                 {this.props.quiz_position <= this.props.quiz.NumberOfQuestions ?
                     <h2>Question # {this.props.quiz_position} out of {this.props.quiz.NumberOfQuestions} </h2>
                 : null}
+                {this.props.quiz.IsInReviewMode ?
+                <h2>This Question/Answer is : {this.getResult()}
+                </h2>
+
+                :null
+                
+                }
             </div>
         )
     }
